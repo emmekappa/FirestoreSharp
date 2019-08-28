71c138c3662989ee34497105cd417325557172bd
Michele Cantelli <emmekappa@gmail.com>
2019-08-28 18:45:21 +0200

    FIRESTORE_EMULATOR_HOST env var support.

--- FirestoreSharp.Tests/FirestoreDbFactory.cs
+++ FirestoreSharp.Tests/FirestoreDbFactory.cs
@@ -1,5 +1,7 @@
 namespace FirestoreSharp.Tests
 {
+    using System;
+    using System.Text.RegularExpressions;
     using Google.Api.Gax.Grpc;
     using Google.Cloud.Firestore;
     using Google.Cloud.Firestore.V1;
@@ -9,12 +11,17 @@ namespace FirestoreSharp.Tests
     {
         public static FirestoreDb Create()
         {
+            var firestoreHost = Environment.GetEnvironmentVariable("FIRESTORE_EMULATOR_HOST") ?? "localhost:8080";
+            var matches = Regex.Match(firestoreHost, "(?<host>.*):(?<port>\\d+)");
+            var host = matches.Groups["host"].Value;
+            var port = int.Parse(matches.Groups["port"].Value);
+
             var firestoreSettings = new FirestoreSettings
             {
                 CallSettings = CallSettings.FromHeaderMutation(x => x.Add("Authorization", "Bearer owner"))
             };
             return FirestoreDb.Create("na",
-                FirestoreClient.Create(new Channel("127.0.0.1", 8080, ChannelCredentials.Insecure), firestoreSettings));
+                FirestoreClient.Create(new Channel(host, port, ChannelCredentials.Insecure), firestoreSettings));
         }
     }
 }
